bc7349f53be382b0f7227d1bdb2cccdd5641179d
Donald Mwanga <donaldmwanga33.com>
2023-01-03 23:12:49 +0000

    printing result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 3-infinite_add.py
+# Donald Mwanga <donaldmwanga33@gmail.com
+
+if __name__ == "__main__":
+    """print the addition of all arguments"""
+
+    import sys
+
+    total = 0
+    for i in range (len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
