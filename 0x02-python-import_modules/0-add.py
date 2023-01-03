b0327633af4451e71d9883153441c59dacf33557
Donald Mwanga <donaldmwanga33.com>
2023-01-03 14:11:03 +0000

    sum of two numbers

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File : 0_add.py
+# Author : Donald Mwanga <donaldmwanga33@gmail.com>
+
+if __name__ == "__main__":
+
+    """ print the sum of two numbers"""
+
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}". format (a, b, add(a,b)))
