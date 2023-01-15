ee6b53d44d6dec18d7681ebac14f95b7f921581b
Donald Mwanga <donaldmwanga33.com>
2023-01-15 03:19:11 +0000

    replacing an element in a list at a specified position

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+# Donald Mwanga <donaldmwanga33@gmail.com>
+
+
+def replace_in_list(my_list, idx, element):
+    """replaces and element of a list in specific position"""
+    if idx >= 0 and  idx < len(my_list):
+        my_list[idx] = element
+        return (my_list)
