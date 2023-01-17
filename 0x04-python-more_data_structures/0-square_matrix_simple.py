3a289b0e5bd202408c99f0bd7bb0c0074c68fe15
Donald Mwanga <donaldmwanga33.com>
2023-01-17 17:38:10 +0000

    computing the square value of all integer of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square_matrix_simple.py
+# Donald Mwanga <donaldmwanga33@gmail.com>
+
+
+def square_matrix_simple(matrix=[]):
+
+    """computes the square value of all integers of a matrix."""
+    return([list(map(lambda x : x * x, row)) for row in matrix])
