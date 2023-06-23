81996b5a1c88b6fac1e048bf36f1f3ea35115480
DonaldMwanga <donaldmwanga33@gmail.com>
2023-06-23 16:12:40 +0000

    empty class BaseGeomety

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Donald Mwanga <donaldmwanga33@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
